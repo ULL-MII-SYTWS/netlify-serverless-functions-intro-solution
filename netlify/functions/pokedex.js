b9a0a0866f7e45b8f7401f93916a162740224fe1
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-12-10 15:12:13 +0000

    04-end-completed

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+const POKE_API = 'https://pokeapi.co/api/v2/'
+
+export const handler = async (event, context) => {
+  const result = await fetch(POKE_API + '/pokemon?limit=3&offset=0')
+  const data = await result.json()
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
+}

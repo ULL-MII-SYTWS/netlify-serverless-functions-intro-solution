7b2b102d033452161f0a1ac33809f109634fc24b
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-12-11 11:01:18 +0000

    pokedex.js console.log added

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -4,6 +4,7 @@ const POKE_API = 'https://pokeapi.co/api/v2/'
 export const handler = async (event, context) => {
   const result = await fetch(POKE_API + '/pokemon?limit=3&offset=0')
   const data = await result.json()
+  console.log(data)
   return {
     statusCode: 200,
     body: JSON.stringify(data)
